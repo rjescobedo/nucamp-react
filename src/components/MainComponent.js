6b0c6aaf7067ef88951167b4fb86af7a1e692089
Raul Escobedo <rjescobedo@gmail.com>
2021-12-29 06:57:16 -0600

    React Redux Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,7 +11,7 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
     return {
-        campsite: state.campsites,
+        campsites: state.campsites,
         comments: state.comments,
         partners: state.partners,
         promotions: state.promotions
